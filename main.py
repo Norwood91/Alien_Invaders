90e0c0df51a07909db98b052130219b757f33ccf
Robert Norwood <you@example.com>
2022-08-18 15:16:40 -0700

    Updated the ship class by adding an attribute 'moving_right' and a method called update. Touched up the check_events method to allow the ship to move continuously right on a key press.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 import sys
 from settings import Settings
+from ship import Ship
 
 
-class Alien_invader:
+class AlienInvader:
     def __init__(self):
         # Initializes the background settings that pygame needs to work properly.
         pygame.init()
@@ -11,24 +12,41 @@ class Alien_invader:
 
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption('Alien Invaders')
-        # Set background color of the screen
-        self.bg_color = (230, 230, 230)
+        # The self argument refers to the current instance of Alien_invader
+        # This is the param that gives Ship access to the game's resources, like the screen object
+        self.ship = Ship(self)
 
     def run_game(self):
         while True:
-            # Watch for keyboard and mouse events.
-            # Pygame.event.get() returns a list of events that have taken place since the last time the function was
-            # called.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            # Redraw the screen during each pass through of the loop
-            self.screen.fill(self.settings.bg_color)
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._check_events()
+            # This will update the ship's position after we've checked for keyboard events and before we update screen
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
+        # Pygame.event.get() returns a list of events that have taken place since the last time the function was
+        # called.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            # If you stop pressing the right arrow button (KEYUP means stop pressing button)
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
-    ai = Alien_invader()
+    ai = AlienInvader()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -9,11 +9,18 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect() # Places ship in current location on screen
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/goku.bmp') # Returns a surface representing the ship
+        self.image = pygame.image.load('images/ship.bmp') # Returns a surface representing the ship
         self.rect = self.image.get_rect()
 
         # Draws and starts each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+
+    def update(self):
+        # Update the ships position based on the movement flag
+        # Move the ship to the right by increasing the ship's rect.x value by 1 pixel
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         # Draw the ship at its current location
